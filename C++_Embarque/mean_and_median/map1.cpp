20b2cf198789910a2d50121d9fffc43a254ad9a6
RomainCocogne <romain.cocogne83@gmail.com>
2020-02-10 18:40:28 +0100

    map1 c++ embarqué

--- C++_Embarque/mean_and_median/map1.cpp
+++ C++_Embarque/mean_and_median/map1.cpp
@@ -1,14 +1,14 @@
-//
+// Copyright 2015 ng2goodies
 // header-start
 //////////////////////////////////////////////////////////////////////////////////
 //
-// \file      mean_and_median.cpp
+// \file      histogram.cpp
 //
 // \brief     This file belongs to the C++ tutorial project
 //
 // \author    Bernard
 //
-// \copyright Copyright ng2goodies 2015
+// \copyright Copyright [2015] <ng2goodies>
 //            Distributed under the MIT License
 //            See http://opensource.org/licenses/MIT
 //
@@ -18,15 +18,15 @@
 
 
 // C++ version
-// Compilation with g++ 5.3.0 & g++ 6.3.0 
+// Compilation with g++ 5.3.0 & g++ 6.3.0
 // mingwin: g++ -std=c++14 -O3 -o mean_and_median mean_and_median.cpp
 
 // Compilation with g++ 4.9.3
 // cygwin:  g++ -D_GLIBCXX_USE_C99 -D_GLIBCXX_USE_C99_DYNAMIC -std=c++14 -O3 ...
 
-// Other tool chain 
+// Other tool chain
 // msvc:    ok!
-// clang:   ok! 
+// clang:   ok!
 
 
 #include <string>
@@ -34,9 +34,12 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
+#include <iomanip>
+#include <map>
 
 using std::string;
 using std::vector;
+using std::map;
 
 // This simple program reads from a file a set of numbers (double format)
 // computes a running mean value, computes the median after sort
@@ -44,26 +47,38 @@ using std::vector;
 // This program has several problems, can you spot them
 
 int main(int argc, char *argv[]) {
-  string file_name{argv[1]};
-  vector<double> buf;
-  std::ifstream fin(file_name, std::ios::in);
-
-  string line;
-  auto mean = 0.0;
-
-  while (std::getline(fin, line)) {
-    auto d = std::stod(line);
-    buf.push_back(d);
-    mean = (buf.size() == 1) ? d : mean + (d - mean) / buf.size();
+  if (argc != 2) {
+    std::cerr << "Error, need a file arg" << std::endl;
+    return -1;
   }
 
-  std::sort(buf.begin(), buf.end());
+  string file_name{argv[1]};
+  map<string, double> buf;
+  std::ifstream fin(file_name, std::ios::in);
+  if (!fin.is_open()) {
+    std::cerr << "Error, invalid name file"<< std::endl;
+    return -1;
+  }
 
-  auto mid = buf.size() / 2;
-  double median = (buf.size() % 2) ? buf[mid] :
-                                     (buf[mid - 1] + buf[mid]) / 2;
+  string line;
+  while (std::getline(fin, line)) {
+    auto key = line.substr(0, line.find(" "));
+    auto d = std::stod(line.substr(line.find(" "), line.length()));
+    buf[key] = d;
+  }
 
-  std::cout << "number of elements = " << buf.size()
-            << ", median = " << median
-            << ", mean = " << mean << std::endl;
+  string qin;
+  for (;;) {
+    std::cout << "query> ";
+    std::cin >> qin;
+
+    if (qin == "END") break;
+
+    if (buf.find(qin) != buf.end())
+      std::cout << "value[" << qin << "]= " << buf[qin] << std::endl;
+    else
+      std::cout << "This ID does not exists" << std::endl;
+  }
+
+  std::cout << "Bye..." << std::endl;
 }
